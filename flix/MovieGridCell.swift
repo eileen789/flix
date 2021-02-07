7ba7a9e7939c3ff4d4763fb2ff9025a15f5b91da
Eileen <ehuang053@gmail.com>
2021-02-06 20:49:36 -0500

    collection view

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Eileen Huang on 2/6/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
